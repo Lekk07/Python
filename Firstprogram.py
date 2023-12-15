ebaec815becf69f060fffdb1574da89a89e34377
Lekk07 <148355584+Lekk07@users.noreply.github.com>
2023-12-15 22:20:50 +0530

    Create Firstprogram.py

---
+++ Firstprogram.py
@@ -0,0 +1,37 @@
+print("Hello World")
+name="lekk"
+print(name)
+
+name="Tony stark"
+age="51"
+print(name)
+print(age)
+is_genius=True
+print(is_genius)
+
+name= input("what is your name? ")
+print("Hello " +name)
+
+
+first=input("enter first number :")
+second=input("enter second number :")
+sum= int(first)+ int(second)
+print("the sum is  :" + str(sum))
+
+name="Tony stark"
+
+#here is a comment
+
+print(3>2)
+print(6!=2)
+
+print(3>2 and 2>6)
+print(1>2 or 2>1)
+
+age=19
+if age >=18:
+    print("you are an adult")
+
+elif age< 18 and age >3:
+    print("you are a child")
+print("thankyou")
